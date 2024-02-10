e2a995fd8927826fd7ae26f69c19c441f9b44a81
DeveloperJarne <jarne.staal9@outlook.com>
2024-02-11 00:18:41 +0100

    Added currency symbol to trending products

--- src/components/home/TrendingProductsComponent.vue
+++ src/components/home/TrendingProductsComponent.vue
@@ -42,7 +42,9 @@
               </a>
             </h3>
             <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
-            <p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p>
+            <p class="mt-1 text-sm font-medium text-gray-900">
+              {{ $store.state.currency.symbol }} {{ product.price }}
+            </p>
           </div>
         </router-link>
       </div>
